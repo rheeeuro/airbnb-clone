40937b9a358842f20a9105eb4c05f1aa4c1486cd
eurohand <eurohand@naver.com>
2020-03-23 17:12:49 +0900

    Finishing User Model

--- users/models.py
+++ users/models.py
@@ -4,4 +4,38 @@ from django.db import models
 
 class User(AbstractUser):
 
-    pass
+    """ Custom User Model """
+
+    GENDER_MALE = "male"
+    GENDER_FEMALE = "female"
+    GENDER_OTHER = "other"
+
+    GENDER_CHOICE = (
+        (GENDER_MALE, "남성"),
+        (GENDER_FEMALE, "여성"),
+        (GENDER_OTHER, "기타"),
+    )
+
+    LANGUAGE_ENGLIGH = "en"
+    LANGUAGE_KOREAN = "kr"
+
+    LANGUAGE_CHOICE = ((LANGUAGE_ENGLIGH, "English"), (LANGUAGE_KOREAN, "한국어"))
+
+    CURRENCY_USD = "usd"
+    CURRENCY_KRW = "krw"
+
+    CURRENCY_CHOICE = ((CURRENCY_USD, "USD"), (CURRENCY_KRW, "KRW"))
+
+    avatar = models.ImageField(null=True, blank=True)
+    gender = models.CharField(
+        choices=GENDER_CHOICE, max_length=10, null=True, blank=True
+    )
+    bio = models.TextField(default="", blank=True)
+    birthday = models.DateField(null=True)
+    language = models.CharField(
+        choices=LANGUAGE_CHOICE, max_length=2, null=True, blank=True
+    )
+    currency = models.CharField(
+        choices=CURRENCY_CHOICE, max_length=3, null=True, blank=True
+    )
+    superhost = models.BooleanField(default=False)
