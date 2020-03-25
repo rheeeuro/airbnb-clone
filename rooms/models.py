792b64c30db84b0f29788a55dc4db01e0dc07d2a
eurohand <eurohand@naver.com>
2020-03-25 17:26:04 +0900

    Time Stamped Model

--- rooms/models.py
+++ rooms/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django_countries.fields import CountryField
 
-# Create your models here.
+from core import models as core_models
+from users import models as user_models
+
+
+class Room(core_models.TimeStampedModel):
+
+    """ Room Model Definition """
+
+    name = models.CharField(max_length=140)
+    description = models.TextField()
+    country = CountryField()
+    city = models.CharField(max_length=80)
+    price = models.IntegerField()
+    address = models.CharField(max_length=140)
+    beds = models.IntegerField()
+    bedrooms = models.IntegerField()
+    baths = models.IntegerField()
+    guests = models.IntegerField()
+    check_in = models.TimeField()
+    check_out = models.TimeField()
+    instant_book = models.BooleanField(default=False)
+    host = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
